156f3c07d11f737b479a0de2f0ab6ac1c45f2bb4
Ghost-of-LA <116952572+Ghost-of-LA@users.noreply.github.com>
2022-11-10 16:54:53 -0800

    Comments; Added planetary station type
    Added comments and added another planetary station type to check for if the event is Undocked.

--- load.py
+++ load.py
@@ -41,12 +41,12 @@ VERSION = '3.1.0'
 
 this = sys.modules[__name__]  # For holding module globals
 
-czLocation = ""
-superCruise = ""
-bodyName = ""
-taxiDestination = ""
-stationGuess = ""
-srvName = ""
+czLocation = "" # To hold combat zone location
+superCruise = "" # To hold flag for supercruising
+bodyName = "" # To hold body name
+taxiDestination = "" # To hold taxi destination
+stationGuess = "" # To hold station name if EDMC does not detect a station
+srvName = "" # To hold name of SRV model
 
 def callback(result):
     logger.info(f'Callback: {result}')
@@ -141,179 +141,203 @@ def journal_entry(cmdr, is_beta, system, station, entry, state):
     presence_details = this.presence_details
     
     if entry['event'] == 'Location':
-        if "OnFoot" in entry:
-            if entry['OnFoot'] == True:
-                if entry['BodyType'] == "Station":
+        if "OnFoot" in entry: # Check If the entry contains the OnFoot boolean
+            if entry['OnFoot'] == True: # If player is on foot
+                if entry['BodyType'] == "Station": # If the player is at a station
                     presence_details = 'On foot at ' + entry["Body"]
-                elif entry['BodyType'] == "Planet":
+                elif entry['BodyType'] == "Planet": # If the player is on a planet surface
                     presence_details = 'On foot on ' + entry["Body"]
-        else:
-            if entry['Docked'] == True:
+        else: # If player is not on foot
+            if entry['Docked'] == True: # If ship is docked
                     presence_details = 'Docked at ' + entry["StationName"]
-            elif entry['Docked'] == False:
-                if entry['Taxi'] == True:
-                    if state['Dropship'] == True:
+            elif entry['Docked'] == False: # If ship is not docked
+                if entry['Taxi'] == True: # If player is in a taxi
+                    if state['Dropship'] == True: # If Frontline Solutions
                         presence_details = 'Taking a dropship back to ' + taxiDestination
-                    else:
-                        if taxiDestination != "":
+                    else: # If Apex Interstellar
+                        if taxiDestination != "": # If taxi destination is stored
                             presence_details = 'Taking a taxi to ' + taxiDestination
                         else:
                             presence_details = 'Taking a taxi'
-                elif entry['BodyType'] == "Planet":
+                elif entry['BodyType'] == "Planet": # If ship is on a planet but not docked
                     presence_details = 'On ' + entry["Body"]
-                else:
+                else: # If ship is not docked or on a planet
                     presence_details = 'Flying in normal space'
         presence_state = 'In ' + entry['StarSystem']
-
-    elif entry['event'] == 'Embark':
-        if entry['Taxi'] == True and state['Dropship'] != True:
-            if taxiDestination != "":
+    
+    elif entry['event'] == 'Embark': # Getting into ship or SRV
+        if entry['Taxi'] == True and state['Dropship'] != True: # If Apex Interstellar
+            if taxiDestination != "": # If there is a destination stored
                 presence_details = 'Taking a taxi to ' + taxiDestination
             else:
                 presence_details = 'Taking a taxi'
-        elif state['Dropship'] == True:
-            if czLocation != "":
+        elif state['Dropship'] == True: # If Frontline Solutions
+            if czLocation != "": # If there is a combat zone location stored
                 presence_details = 'Taking a dropship to ' + czLocation
             else:
                 presence_details = 'Taking a dropship'
-        elif entry['OnStation'] == True:
+        elif entry['OnStation'] == True: # If ship is at a station but not a taxi or dropship
             presence_details = 'Docked at ' + entry['StationName']
-        elif entry['SRV'] == True:
-            if srvName != "":
+        elif entry['SRV'] == True: # If entering SRV
+            if srvName != "": # If SRV name is stored
                 presence_details = 'Driving ' + srvName + ' On ' + entry['Body']
             else:
                 presence_details = 'Driving SRV On ' + entry['Body'] 
-        elif entry['OnPlanet'] == True:
-            if station is not None:
+        elif entry['OnPlanet'] == True: #If on a planet
+            if station is not None: # If EDMC detects the station
                 presence_details = 'Docked at ' + station
-            elif stationGuess != "":
+            elif stationGuess != "": # If station name was stored
                 presence_details = 'Docked at ' + stationGuess
-            else:
+            else: # If station name is unknown or not at a station/settlement
                 presence_details = 'On ' + entry['Body']
         presence_state = 'In ' + entry['StarSystem']
-            
-    elif entry['event'] == 'Disembark':
-        if station is not None:
+                
+    elif entry['event'] == 'Disembark': # Getting out of ship or SRV
+        if station is not None: # If EDMC detects the station
             presence_details = 'On foot at ' + station
-        elif stationGuess != "":
+        elif stationGuess != "": # If station name was stored
             presence_details = 'On foot at ' + stationGuess
-        elif entry['OnStation'] == True:
+        elif entry['OnStation'] == True: # If event entry has a station
             presence_details = 'On foot at ' + entry['StationName']
-        elif entry['OnPlanet'] == True:
+        elif entry['OnPlanet'] == True: # If on a planet, but not at a station/settlement
             presence_details = 'On foot on ' + entry['Body']
-            bodyName = entry['Body']
+            bodyName = entry['Body'] # Store body name
         presence_state = 'In ' + entry['StarSystem']
-        taxiDestination = ""
+        taxiDestination = "" # Clear taxi destination in case player is exiting taxi
         
-    elif entry['event'] == 'LaunchSRV':
-        if entry['PlayerControlled'] == True:
-            if bodyName != "":
+    elif entry['event'] == 'LaunchSRV': # Deploying SRV from ship
+        if entry['PlayerControlled'] == True: # If this action was done by the player
+            if bodyName != "": # If a body name was stored
+                # Strip the first 4 charcaters from the SRV name so instead of "SRV Scarab" we get "Scarab"
                 presence_details = 'Driving ' + entry['SRVType_Localised'][4:] + ' on ' + bodyName
             else:
                 presence_details = 'Driving ' + entry['SRVType_Localised'][4:]
-            srvName = entry['SRVType_Localised'][4:]
+            srvName = entry['SRVType_Localised'][4:] # Store the SRV name
                 
-    elif entry['event'] == 'DockSRV':
-        if bodyName != "":
+    elif entry['event'] == 'DockSRV': # Putting SRV back into ship
+        if bodyName != "": # If body name was stored
             presence_details = 'On ' + bodyName
         else:
             presence_details = 'In ship on planet surface'
-        srvName = ""
+        srvName = "" # Clear SRV name
             
-    elif entry['event'] == "Touchdown":
-        if entry['PlayerControlled'] == True:
+    elif entry['event'] == "Touchdown": # Landing on a planet
+        if entry['PlayerControlled'] == True: # If player and not recalling ship
             presence_details = 'Landed On ' + entry['Body']
             presence_state = 'In ' + entry['StarSystem']
             bodyName = entry['Body']
         
-    elif entry['event'] == "Liftoff":
+    elif entry['event'] == "Liftoff": # Lifting off planet surface
+        # If not a taxi and not dismissing ship
         if state['Taxi'] != True and entry['PlayerControlled'] == True:
             presence_details = 'Flying On ' + entry['Body']
             presence_state = 'In ' + entry['StarSystem']
  
-    elif entry['event'] == "ApproachBody":
+    elif entry['event'] == "ApproachBody": # Entering orbital cruise
+        # Presence Details are not updated here so "Taking a taxi" or "Taking a dropship" persists
+        # Until the player reaches the destination. The system is updated to show progress towards destination.
+        # The superCruise variable is checked so that the supercrusing status is not overwritten if the ship
+        # is still supercruising.
+        # If not a taxi or dropship and not supercruising
         if state['Taxi'] != True and state['Dropship'] != True and superCruise != "Y":
             presence_details = 'Flying On ' + entry['Body']
         presence_state = 'In ' + entry['StarSystem']  
-        bodyName = entry['Body']
+        bodyName = entry['Body'] # Save body name to use for events while on planet
         
-    elif entry['event'] == "LeaveBody":
+    elif entry['event'] == "LeaveBody": # Leaving orbital cruise
+        # Presence Details are not updated here so "Taking a taxi" or "Taking a dropship" persists
+        # Until the player reaches the destination. The system is updated to show progress towards destination.
+        # The superCruise variable is checked so that the supercrusing status is not overwritten if the ship
+        # is still supercruising.
+        # If not a taxi or dropship and not supercruising
         if state['Taxi'] != True and state['Dropship'] != True and superCruise != "Y":
             presence_details = 'Flying in normal space'
         presence_state = 'In ' + entry['StarSystem']
-        bodyName = ""
+        bodyName = "" # Clear body name because player is no longer on the planet
         
-    elif entry['event'] == "Docked":
+    elif entry['event'] == "Docked": # Docking at a station
         presence_details = 'Docked at ' + entry['StationName']
         presence_state = 'In ' + entry['StarSystem']
+        # Store station name, solves issue with planetary station name not showing when boarding ship
         stationGuess = entry['StationName']
         
-    elif entry['event'] == "Undocked":
-        if entry['Taxi'] != True and state['Dropship'] != True:
-            if entry['StationType'] == "OnFootSettlement" or entry['StationType'] == "CraterOutpost":
-                if bodyName == "":
+    elif entry['event'] == "Undocked": # Launching from station
+        # Presence Details are not updated on undocking so "Taking a taxi" or "Taking a dropship" persists
+        # Until the player reaches the destination. The system is updated to show progress towards destination.
+        if entry['Taxi'] != True and state['Dropship'] != True: # If not a taxi or dropship
+            # If planetary station
+            if entry['StationType'] == "OnFootSettlement" or entry['StationType'] == "CraterOutpost" or entry['StationType'] == "CraterPort":
+                if bodyName == "": # If body name is not stored
                     presence_details = 'Leaving ' + entry['StationName']
-                else:
+                else: # If body name was stored
                     presence_details = 'Flying on ' + bodyName
-            else:
+            else: # If space station
                 presence_details = 'Flying in normal space'
         presence_state = 'In ' + system 
-        stationGuess = ""
+        stationGuess = "" # Clear station name since player is leaving station
         
-    elif entry['event'] == 'StartJump':
-        if state['Taxi'] != True and state['Dropship'] != True:
-            if entry['JumpType'] == 'Hyperspace':
-                presence_details = 'Jumping to ' + entry['StarSystem']
+    elif entry['event'] == 'StartJump': # Hyperspace jump or supercruise
+        # Presence Details are not updated on jumping/supercruise so "Taking a taxi" or "Taking a dropship" persists
+        # Until the player reaches the destination. The system is updated to show progress towards destination.
+        if state['Taxi'] != True and state['Dropship'] != True: # If not taxi or dropship
+            if entry['JumpType'] == 'Hyperspace': # If hyperspace jump
+                presence_details = 'Jumping to ' + entry['StarSystem'] # This entry shows destination system
                 presence_state = 'From ' + system
-            elif entry['JumpType'] == 'Supercruise':
+            elif entry['JumpType'] == 'Supercruise': # If supercruise
                 presence_details = 'Preparing for supercruise'
                 presence_state = 'In ' + system
-                superCruise = "Y"
+                superCruise = "Y" # Update superCruise variable
             
-    elif entry['event'] == 'SupercruiseEntry':
-        if state['Taxi'] != True and state['Dropship'] != True:
+    elif entry['event'] == 'SupercruiseEntry': # Entering supercruise
+        # Presence Details are not updated on supercruise so "Taking a taxi" or "Taking a dropship" persists
+        # Until the player reaches the destination. The system is updated to show progress towards destination.
+        if state['Taxi'] != True and state['Dropship'] != True: # If not taxi or dropship
             presence_details = 'Supercruising'
         presence_state = "In " + entry['StarSystem']
-        superCruise = "Y"
+        superCruise = "Y" # Update superCruise variable
         
-    elif entry['event'] == 'SupercruiseExit':
-        if state['Taxi'] != True and state['Dropship'] != True:
-            if entry['BodyType'] == "Planet":
+    elif entry['event'] == 'SupercruiseExit': # Exiting supercruise
+        # Presence Details are not updated on supercruise so "Taking a taxi" or "Taking a dropship" persists
+        # Until the player reaches the destination. The system is updated to show progress towards destination.
+        if state['Taxi'] != True and state['Dropship'] != True: # If not taxi or dropship
+            if entry['BodyType'] == "Planet": # If ship is flying over planet surface / in orbital cruise
                 presence_details = 'Flying On ' + entry['Body']
             else:
                 presence_details = 'Flying in normal space'
         presence_state = 'In ' + entry['StarSystem']
-        superCruise = ""
+        superCruise = "" # clear superCruise variable since player is no longer supercruising
         
-    elif entry['event'] == 'FSDJump':
-        if state['Taxi'] != True and state['Dropship'] != True:
+    elif entry['event'] == 'FSDJump': # Supercruising
+        # Presence Details are not updated on supercruise so "Taking a taxi" or "Taking a dropship" persists
+        # Until the player reaches the destination. The system is updated to show progress towards destination.
+        if state['Taxi'] != True and state['Dropship'] != True: # If not taxi or dropship
             presence_details = 'Supercruising'
         presence_state = 'In ' + entry['StarSystem']
-        superCruise = "Y"
+        superCruise = "Y" # Update superCruise variable
     
-    elif entry['event'] == 'BookDropship':
-        if entry['Retreat'] == False:
-            czLocation = entry['DestinationLocation']
-        elif entry['Retreat'] == True:
-            taxiDestination = entry['DestinationLocation']
+    elif entry['event'] == 'BookDropship': # Dropship coming to pick player up
+        if entry['Retreat'] == False: # Signing up for combat zone at Frontline Solutions desk
+            czLocation = entry['DestinationLocation'] # Store combat zone location (Settlement)
+        elif entry['Retreat'] == True: # End of ground combat zone if dropship was taken
+            taxiDestination = entry['DestinationLocation'] # Store destination (Station name)
 
-    elif entry['event'] == 'CancelDropship':
-        czLocation = ""
+    elif entry['event'] == 'CancelDropship': # Canceling deployment at Frontline Solutions
+        czLocation = "" # Clear combat zone location since deployment canceled
     
-    elif entry['event'] == 'DropshipDeploy':
-        if czLocation == "":
+    elif entry['event'] == 'DropshipDeploy': # Exiting dropship
+        if czLocation == "": # If combat zone location was stored
            presence_details = 'Combat zone on ' + entry['Body']
-        else:
+        else: # If combat zone location was not stored
             presence_details = 'Combat zone at ' + czLocation
         presence_state = 'In ' + system
         
-    elif entry['event'] == 'BookTaxi':
-        taxiDestination = entry['DestinationLocation']
+    elif entry['event'] == 'BookTaxi': # Requesting transport from Apex Interstellar
+        taxiDestination = entry['DestinationLocation'] # Store destination
     
-    elif entry['event'] == 'CancelTaxi':
-        taxiDestination = ""
+    elif entry['event'] == 'CancelTaxi': # Canceling transport from Apex Interstellar
+        taxiDestination = "" # Clear destination since transport is canceled
         
-    elif entry['event'] == 'Shutdown':
+    elif entry['event'] == 'Shutdown': # Exiting game to desktop or main menu
         this.presence_state = _('Connecting CMDR Interface')
         this.presence_details = ''
         stationName = ""
